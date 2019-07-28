e3efe4e0702d719ad575b550f30414928e422e51
etecdados <53099959+etecdados@users.noreply.github.com>
2019-07-28 09:17:40 -0300

    update

---
+++ root/scripts/etecdados.js
@@ -0,0 +1,9 @@
+/*
+ * https://etecdados.github.io
+ * etecdados v1.0.0
+ * copyright 2019 by etecdados
+ * created on 2013/06/24 12:10
+ * author fernando silva
+ * last update on 2019/07/25
+ */
+
